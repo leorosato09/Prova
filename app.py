70b8b66ce668afd2170e3cfc3e9aac4efd9bffcc
leorosato09 <leorosato.rosato@gmail.com>
2024-08-09 12:40:13 +0200

    Speriamo bene

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask, request, render_template_string
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def chiedi_nome():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        return f'Ciao, {nome}!'
-    return '''
-        <form method="post">
-            Inserisci il tuo nome: <input type="text" name="nome">
-            <input type="submit" value="Invia">
-        </form>
-    '''
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
